674a7623e07d4f26512c8bac259c0beba15796da
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-08-26 13:49:48 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/fula_constructor_s/a/s/FULA_CONSTRUCTOR_SAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fula_constructor_s.a.s;
+
+/**
+ *
+ * @author Windows
+ */
+public class FULA_CONSTRUCTOR_SAS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
